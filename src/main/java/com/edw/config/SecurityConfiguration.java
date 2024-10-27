1632d333116481978d87d4e4cd92fa4909d1bb69
Manish Singh <manish.singh@thoughtfocus.com>
2024-10-27 11:46:32 +0530

    modified files

--- src/main/java/com/edw/config/SecurityConfiguration.java
+++ src/main/java/com/edw/config/SecurityConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * <pre>
@@ -34,14 +39,32 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
 
         http
+                .cors() // Enable CORS
+                .and()
+                .csrf().disable() // Disable CSRF for simplicity (ensure it's appropriate for your app)authorizeHttpRequests()
                 .authorizeHttpRequests()
-                            .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
+                .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
                             .anyRequest()
                                 .fullyAuthenticated()
                 .and()
                     .logout()
-                    .logoutSuccessUrl("http://localhost:8080/realms/external/protocol/openid-connect/logout?redirect_uri=http://localhost:8081/");
+                    .logoutSuccessUrl("http://localhost:8080/realms/External/protocol/openid-connect/logout?redirect_uri=http://localhost:8081/");
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        //configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5173", "http://localhost:8080")); // Allow your Vue.js app
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Allow your Vue.js app
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // If you need to include credentials (cookies, etc.)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
